ff1ca432142f3adbe265daa5b2fee54023d02416
Pia Cuk <pia.cuk@gmx.de>
2022-01-18 13:58:56 +0100

    Always keep output layer if 1000 classes

--- distillistic/models/resnet.py
+++ distillistic/models/resnet.py
@@ -8,7 +8,7 @@ class CustomResNet(nn.Module):
     See https://github.com/Stevellen/ResNet-Lightning/blob/master/resnet_classifier.py
     """
 
-    def __init__(self, num_classes, resnet_version, pretrained=False, last_layer_only=False, keep_last_layer=False) -> None:
+    def __init__(self, num_classes, resnet_version, pretrained=False, last_layer_only=False) -> None:
         super().__init__()
 
         self.__dict__.update(locals())
@@ -19,7 +19,7 @@ class CustomResNet(nn.Module):
         }
         # Using a pretrained ResNet backbone
         self.resnet_model = resnets[resnet_version](pretrained=pretrained)
-        if (num_classes != 1000) or (not keep_last_layer):
+        if (num_classes != 1000):
             # Replace old FC layer with Identity so we can train our own
             print(f"Creating new output layer with {num_classes} classes.")
             linear_size = list(self.resnet_model.children())[-1].in_features
@@ -39,8 +39,7 @@ def resnet18(num_classes, pretrained=False, last_layer_only=False):
     return CustomResNet(num_classes=num_classes,
                         resnet_version=18,
                         pretrained=pretrained,
-                        last_layer_only=last_layer_only,
-                        keep_last_layer=False
+                        last_layer_only=last_layer_only
                         )
 
 
@@ -48,6 +47,5 @@ def resnet50(num_classes, pretrained=False, last_layer_only=False):
     return CustomResNet(num_classes=num_classes,
                         resnet_version=50,
                         pretrained=pretrained,
-                        last_layer_only=last_layer_only,
-                        keep_last_layer=False
+                        last_layer_only=last_layer_only
                         )
